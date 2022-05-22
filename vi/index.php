9fe5838b4da10980b85563c6000c210934842896
GoldDominik893 <68468388+GoldDominik893@users.noreply.github.com>
2022-05-22 13:35:22 +0100

    Update index.php

--- vi/index.php
+++ vi/index.php
@@ -21,6 +21,6 @@ parse_str($url_components['query'], $params);
         </video></center>
         <center> <h2> <?php echo $params['t']; ?> </h2> </center>
         <center> <h4> <?php echo $params['d']; ?> </h4> </center>
-        <title><?php echo $params['t']; ?>Bad YouTube</title>
+        <title><?php echo $params['t']; ?> | Bad YouTube</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <link rel="stylesheet" href="/styles/player.css">
