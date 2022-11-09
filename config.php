bf990dceb4f219269404b5b384af3fe386a5483d
Dominik Wajda <admin@epicsite.xyz>
2022-11-09 21:27:33 +0000

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php 
+    /** ALL FIELDS ARE REQUIRED */
+    $servername = "localhost"; /** The IP / domain used to connect to your database */
+    $username = "root"; /** The database username */
+    $password = ""; /** The database password */
+    $dbname = "users"; /** The database name, by default, if you imported database.sql it is "users" */
+    $adminuser = "GoldDominik893"; /** The user on badyt you want to have access to the admin dashboard */
+    $ytapikey = "YOUR_API_KEY"; /** Your YouTube API v3 Key */
+?>
