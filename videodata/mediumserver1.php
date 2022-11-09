6040ff799c36bce87843254c45eb81a1191726ec
Dominik Wajda <admin@epicsite.xyz>
2022-11-09 21:32:07 +0000

    Add files via upload

---
+++ videodata/mediumserver1.php
@@ -0,0 +1,13 @@
+<?php
+    if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
+        $link = "https";
+    else $link = "http";
+    $link .= "://";
+    $link .= $_SERVER['HTTP_HOST'];
+    $link .= $_SERVER['REQUEST_URI'];
+$url = $link;
+$url_components = parse_url($url);
+parse_str($url_components['query'], $params);
+header('Location: https://yewtu.be/latest_version?id=' . $params['id'] . '&itag=18');
+exit;
+?>
