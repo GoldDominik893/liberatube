a62ff69c07ec45594fd20d43d13ec7e7fea6ba67
Dominik Wajda <admin@epicsite.xyz>
2022-11-09 21:27:42 +0000

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,37 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 09, 2022 at 09:59 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+--
+-- Database: `users`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `username` varchar(256) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `salt1` varchar(12) NOT NULL,
+  `salt2` varchar(12) NOT NULL,
+  `theme` varchar(30) NOT NULL,
+  `lang` varchar(30) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `login`
+--
+
+COMMIT;
