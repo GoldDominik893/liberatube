a403e7fca83c61f1717cf2fd0442078aeab552b9
Dominik Wajda <admin@epicsite.xyz>
2023-07-23 15:11:07 +0100

    Issue #15

--- images.php
+++ images.php
@@ -1,3 +1,3 @@
 <?php
-header('Location: https://golddominik893.github.io/file-hosting/images/'.mt_rand(1, 19).'.jpg');
+header('Location: https://golddominik893.github.io/file-hosting/images/'.mt_rand(1, 15).'.jpeg');
 ?>
