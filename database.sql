bf13d0a4680c116a4a9e6cb3fca98c0bd79663de
Dominik Wajda <admin@epicsite.xyz>
2024-01-12 18:32:30 +0000

    add create users db to db file

--- database.sql
+++ database.sql
@@ -1,26 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 5.1.3
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Nov 04, 2022 at 05:11 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.5
+-- Create the users database
+CREATE DATABASE IF NOT EXISTS users;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+-- Switch to the users database
+USE users;
 
---
--- Database: `users`
---
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `login`
---
-
 CREATE TABLE `login` (
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
@@ -34,4 +18,3 @@ CREATE TABLE `login` (
   `videoshadow` varchar(8) NOT NULL,
   `loadcomments` varchar(12) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
-COMMIT;
