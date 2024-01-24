fead007c6abc2edffcf034239a73c0cb7975ea03
Dominik Wajda <admin@epicsite.xyz>
2024-01-24 21:41:21 +0000

    fix captions

--- videodata/captions.php
+++ videodata/captions.php
@@ -1,24 +1,7 @@
 <?php
 include('../config.php');
 header('Content-Type: text/plain');
-    if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
-        $link = "https";
-    else $link = "http";
-    $link .= "://";
-    $link .= $_SERVER['HTTP_HOST'];
-    $link .= $_SERVER['REQUEST_URI'];
-$url = $link;
-$url_components = parse_url($url);
-parse_str($url_components['query'], $params);
 
-if ($params['server'] == "1") {
-    $server = $InvVDCAServer1;
-} elseif ($params['server'] == "2") {
-    $server = $InvVDCAServer2;
-}
-$filename = $server.'/api/v1/captions/' . $params['id'] . '?label=English%20(auto-generated)';
+$filename = $InvVIServer.$_GET['c_ext'];
 $content = file_get_contents($filename); 
-if (str_contains($content, 'captions')) { 
     echo $content;
-}
-?>
