792c519031378feedf201828ae06dd6ec96cf5ac
Dominik Wajda <admin@epicsite.xyz>
2024-04-01 12:34:04 +0100

    update DB structure to use DEFAULT NULL values

--- database.sql
+++ database.sql
@@ -10,15 +10,15 @@ CREATE TABLE `login` (
   `password` varchar(255) NOT NULL,
   `salt1` varchar(12) NOT NULL,
   `salt2` varchar(12) NOT NULL,
-  `theme` varchar(30) NOT NULL,
-  `customtheme_player_url` varchar(125) NOT NULL,
-  `customtheme_home_url` varchar(125) NOT NULL,
-  `lang` varchar(30) NOT NULL,
-  `region` varchar(4) NOT NULL,
-  `proxy` varchar(8) NOT NULL,
-  `player` varchar(15) NOT NULL,
-  `videoshadow` varchar(8) NOT NULL,
-  `loadcomments` varchar(12) NOT NULL,
+  `theme` varchar(30) DEFAULT NULL,
+  `customtheme_player_url` varchar(125) DEFAULT NULL,
+  `customtheme_home_url` varchar(125) DEFAULT NULL,
+  `lang` varchar(30) DEFAULT NULL,
+  `region` varchar(4) DEFAULT NULL,
+  `proxy` varchar(8) DEFAULT NULL,
+  `player` varchar(15) DEFAULT NULL,
+  `videoshadow` varchar(8) DEFAULT NULL,
+  `loadcomments` varchar(12) DEFAULT NULL,
   PRIMARY KEY (`username`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
@@ -27,7 +27,7 @@ CREATE TABLE `playlist` (
   `playlist_id` int NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
   `playlist_name` varchar(255) NOT NULL,
-  `public` varchar(6) NOT NULL,
+  `public` varchar(6) DEFAULT NULL,
   `video_ids` json NOT NULL,
   PRIMARY KEY (`playlist_id`),
   FOREIGN KEY (`username`) REFERENCES `login` (`username`) ON DELETE CASCADE
