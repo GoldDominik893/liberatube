934f257803ef5d3357d6ef58ac31f7aef6b18cbd
Dominik Wajda <admin@epicsite.xyz>
2024-04-12 00:01:04 +0100

    add latest changes

--- index.php
+++ index.php
@@ -325,7 +325,7 @@ else {
         <br><div class="videos-data-container footer w3-animate-left">
             Liberatube Version 1.9 beta · Licensed under AGPLv3 on GitHub · Credits: Dominic Wajda (GoldDominik893).<br>
             This website is optimised for mobile users and does not collect any user data apart from<br> watch history which doesn't exist yet and you will be able to turn it off when logged in.
-            <br><a href="https://matrix.to/#/#libreratube:matrix.org">Join the Matrix</a> <a href="https://discord.gg/z4cCk5c5Zj">or discord</a> · <a href="https://invidious.io">Invidious</a> · <a href="https://github.com/GoldDominik893/liberatube">GitHub</a> · <a href="/donate.html">Donate to the Liberatube project</a> · <a href="https://liberatube-docs.epicsite.xyz/general/4.privacy/">Privacy Policy</a><br>
+            <br><a href="https://matrix.to/#/#libreratube:matrix.org">Join the Matrix</a> <a href="https://discord.gg/z4cCk5c5Zj">or discord</a> · <a href="https://invidious.io">Invidious</a> · <a href="https://github.com/GoldDominik893/liberatube">GitHub</a> · <a href="https://epicsite.xyz#donate">Donate to the Liberatube project</a> · <a href="https://liberatube-docs.epicsite.xyz/general/4.privacy/">Privacy Policy</a><br>
             Have you noticed a bug or want to see a new feature? <a href="https://github.com/GoldDominik893/liberatube/issues">Open an issue on GitHub</a>
             </div>
         </div>
--- scripts/password_validator.js
+++ scripts/password_validator.js
@@ -6,9 +6,7 @@ var length = document.getElementById("length");
 myInput.onfocus = function() {
   document.getElementById("message").style.display = "block";
 }
-myInput.onblur = function() {
-  document.getElementById("message").style.display = "none";
-}
+
 myInput.onkeyup = function() {
   var lowerCaseLetters = /[a-z]/g;
   if(myInput.value.match(lowerCaseLetters)) {  
