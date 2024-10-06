451b9a521253f734adb5fd4b936201c788312d0c
Dominik Wajda <admin@epicsite.xyz>
2024-10-06 21:26:52 +0100

    use a good video info server

--- config.php
+++ config.php
@@ -16,16 +16,16 @@ $dbname = "users";                                // The database name, by defau
 
 // Invidious
 $InvCServer = "https://invidious.perennialte.ch";  // The Invidious instance that will be used to fetch comments
-$InvVIServer = "https://invidious.nerdvpn.de"; // The Invidious instance that will be used to fetch video information
-$InvTServer = "https://invidious.perennialte.ch";  // The Invidious instance that will be used to fetch trending videos for the home page
+$InvVIServer = "https://invi.susurrando.com";      // The Invidious instance that will be used to fetch video information
+$InvTServer = "https://invidious.perennialte.ch";       // The Invidious instance that will be used to fetch trending videos for the home page
 $InvSServer = "https://invidious.perennialte.ch";  // The Invidious instance that will be used to search for content
 
 // Admin and Defaults
-$defaultRegion = "GB";                            // If the user is logged out or doesn't have a region set this will be the default, (put any 2 digit country code)
-$defaultTheme = "ultra-dark";                     // If the user is logged out or doesn't have a theme set this will be the default, (blue / ultra-dark)
-$defaultLang = "en";                              // If the user is logged out or doesn't have a language set this will be the default
-$defaultLoadCommentsSetting = "nothing";          // If the user is logged out or doesn't have a comments loading preference set this will be the default (nothing / noreplies / showall)
-$adminuser = "GoldDominik893";                    // The user on liberatube you want to have access to the admin dashboard
-$testinstance = true;                             // Whether this is a test instance. A disclaimer will be shown, (true / false)
-$allowProxy = "false";                            // Choose if the users can proxy video data through the server, (true / false / downloads)
-$useReturnYTDislike = true;                       // Choose whether the server contacts the return youtube dislike api for an estimate of the dislikes, (true / false)
+$defaultRegion = "GB";                             // If the user is logged out or doesn't have a region set this will be the default, (put any 2 digit country code)
+$defaultTheme = "ultra-dark";                      // If the user is logged out or doesn't have a theme set this will be the default, (blue / ultra-dark)
+$defaultLang = "en";                               // If the user is logged out or doesn't have a language set this will be the default
+$defaultLoadCommentsSetting = "nothing";           // If the user is logged out or doesn't have a comments loading preference set this will be the default (nothing / noreplies / showall)
+$adminuser = "GoldDominik893";                     // The user on liberatube you want to have access to the admin dashboard
+$testinstance = true;                              // Whether this is a test instance. A disclaimer will be shown, (true / false)
+$allowProxy = "false";                             // Choose if the users can proxy video data through the server, (true / false / downloads)
+$useReturnYTDislike = true;                        // Choose whether the server contacts the return youtube dislike api for an estimate of the dislikes, (true / false)
